706bacb2822db50914d9d184421d45b7e48b6b87
Gyanaranjan <g44sahoo@gmail.com>
2024-11-27 06:59:31 +0530

    feat: Enhance string calculator to support multiple numbers, new lines, and custom delimiters

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -15,3 +15,18 @@ test('should return the sum of two numbers', () => {
     expect(add("1,2")).toBe(3);
     expect(add("10,20")).toBe(30);
 });
+
+test('should return the sum of multiple numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+    expect(add("10,20,30")).toBe(60);
+});
+
+test('should handle new lines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("10\n20\n30")).toBe(60);
+});
+
+test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//|\n1|2|3")).toBe(6);
+});
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -2,6 +2,15 @@ export function add(numbers: string): number {
     if (numbers === "") {
         return 0;
     }
-    const numArray = numbers.split(/,|\n/).map(Number);
+
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf("\n");
+        const customDelimiter = numbers.substring(2, delimiterEndIndex);
+        delimiter = new RegExp(customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+        numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    const numArray = numbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
 }
