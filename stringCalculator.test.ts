daba14597a1e45a6bcf68a2231bb8854ab772449
Gyanaranjan <g44sahoo@gmail.com>
2024-11-27 07:05:53 +0530

    feat: Support multiple custom delimiters in string calculator

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -35,3 +35,8 @@ test('should throw an exception for negative numbers', () => {
     expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
     expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2,-3");
 });
+
+test('should support multiple delimiters', () => {
+    expect(add("//[;][|]\n1;2|3")).toBe(6);
+    expect(add("//[;][|]\n1|2;3")).toBe(6);
+});
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -6,8 +6,13 @@ export function add(numbers: string): number {
     let delimiter = /,|\n/;
     if (numbers.startsWith("//")) {
         const delimiterEndIndex = numbers.indexOf("\n");
-        const customDelimiter = numbers.substring(2, delimiterEndIndex);
-        delimiter = new RegExp(customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+        let customDelimiter = numbers.substring(2, delimiterEndIndex);
+        if (customDelimiter.startsWith("[")) {
+            customDelimiter = customDelimiter.slice(1, -1).split("][").map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|");
+        } else {
+            customDelimiter = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        }
+        delimiter = new RegExp(customDelimiter);
         numbers = numbers.substring(delimiterEndIndex + 1);
     }
 
