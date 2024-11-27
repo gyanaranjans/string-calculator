23571320ed58f3796232b62dc449f07b86f90c2d
Gyanaranjan <g44sahoo@gmail.com>
2024-11-27 06:52:53 +0530

    feat: Add functionality to handle single numbers and sum multiple numbers

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -2,5 +2,6 @@ export function add(numbers: string): number {
     if (numbers === "") {
         return 0;
     }
-    return 0;
+    const numArray = numbers.split(/,|\n/).map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
