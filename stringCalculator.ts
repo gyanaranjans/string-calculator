d88c9fade6b58c0cfae149734e3d1fc2413bff64
Gyanaranjan <g44sahoo@gmail.com>
2024-12-05 11:19:44 +0530

    feat: refactoring Negative number and Delimiter

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -4,27 +4,43 @@ export function add(numbers: string): number {
     }
 
     let delimiter = /,|\n/;
-    if (numbers.startsWith("//")) {
-        const delimiterEndIndex = numbers.indexOf("\n");
-        let customDelimiter = numbers.substring(2, delimiterEndIndex);
-        if (customDelimiter.startsWith("[")) {
-            customDelimiter = customDelimiter.slice(1, -1).split("][").map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|");
-        } else {
-            customDelimiter = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        }
-        delimiter = new RegExp(customDelimiter);
-        numbers = numbers.substring(delimiterEndIndex + 1);
-    }
+
+
+    ({ numbers, delimiter } = extractAndReplaceDelimiter(numbers, delemitorReplace, delimiter));
 
     const numArray = numbers.split(delimiter).map(num => {
         const parsedNum = Number(num);
         return isNaN(parsedNum) ? 0 : parsedNum;
     });
+    NegativeNumberCheck(numArray);
+
+    return numArray.reduce((sum, num) => sum + num, 0);
+}
+
+
+function extractAndReplaceDelimiter(numbers: string, delemitorReplace: (delimiter: string) => string, delimiter: RegExp) {
+    if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf("\n");
+        let customDelimiter = numbers.substring(2, delimiterEndIndex);
+        if (customDelimiter.startsWith("[")) {
+            customDelimiter = customDelimiter.slice(1, -1).split("][").map(d => delemitorReplace(d)).join("|");
+        } else {
+            customDelimiter = delemitorReplace(customDelimiter);
+        }
+        delimiter = new RegExp(customDelimiter);
+        numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+    return { numbers, delimiter };
+}
+
+function NegativeNumberCheck(numArray: number[]) {
     const negativeNumbers = numArray.filter(num => num < 0);
 
     if (negativeNumbers.length > 0) {
         throw new Error(`negative numbers not allowed: ${negativeNumbers.join(",")}`);
     }
-
-    return numArray.reduce((sum, num) => sum + num, 0);
 }
+const delemitorReplace = (delimiter: string) => {
+    return delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
